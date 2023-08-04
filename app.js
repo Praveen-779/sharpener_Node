3eb9101e51b0e284b8cbc837ed6cf68f7dea1b0d
Praveen <Praveenpravi779@gmail.com>
2023-08-04 22:10:56 +0530

    main file without routing files

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+const contactusRoutes = require('./routes/contactUs.js')
+const successRoutes = require('./routes/success.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin' ,adminRoutes);
-app.use('/shop',shopRoutes);
+app.use('/',shopRoutes);
+app.use('/',contactusRoutes);
+app.use('/',successRoutes);
 
 app.use((req,res,next)=> {
-    res.status(404).send( '<h1>page not found</h1>' )
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
-app.listen(3000)
+app.listen(3000);
