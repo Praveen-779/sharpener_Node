54f5047cdf5e605e5ae90e0ae880787e6cbd7b8f
Praveen <Praveenpravi779@gmail.com>
2023-12-15 13:14:20 +0530

    routers and filters

--- app.js
+++ app.js
@@ -45,17 +45,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><input type="text" name="size"></input><button type="submit">send product</button> </form> ')
-});
+app.use('/admin',adminRoutes);
 
-app.post('/product',(req,res,next) => {
-    console.log(req.body);
-    res.end();
+app.use('/shop',shopRoutes);
+
+app.use( (req,res,next) => {
+    res.status(404).send('page not found');
 })
 
 app.listen(3000);
